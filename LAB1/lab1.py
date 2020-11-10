a129b08d8d9c151fb04d1a40d4fd36fc3c423e44
imstevenpm.study <imstevenpm.study@gmail.com>
2020-11-10 12:22:56 +0100

    initial commit check

---
+++ LAB1/lab1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import sys
+import cv2 as cv
+import numpy as np
+import matplotlib as matp
+
+print("Python3 version:",sys.version,"\n")
+print("OpenCV version:",cv.__version__,"\n")
+print("Numpy version:",np.__version__,"\n")
+print("Matplotlib version:",matp.__version__,"\n")
